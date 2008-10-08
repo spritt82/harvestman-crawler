68977ecb18408ac3aaf756c1405e0b22ada41e6b
szybalski <szybalski@5678707c-c24f-0410-97b0-47dc362fc966>
2008-10-08 00:15:38 +0000

    Harvestman-branch is closed. all further development will happen in trunk

--- branches/HarvestMan-topaste/HarvestMan/doc/harvestman-epydoc.sh
+++
@@ -1,4 +0,0 @@
-#Generate epydoc documentation from harvest man.
-#Required epydoc >2.0
-#Run as: sh harvestman-epydoc.sh
-epydoc -v -o harvestman-epydoc --name epydoc --css white                  --url http://localhost/ --inheritance listed  --graph classtree ../harvestman --no-frames
